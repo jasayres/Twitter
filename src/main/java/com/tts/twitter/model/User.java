04ae43588db695ba11b15332e88cd2d0ba54d4dc
JSayres <jen.sayres@gmail.com>
2020-10-28 12:48:29 -0500

    added features

--- src/main/java/com/tts/twitter/model/User.java
+++ src/main/java/com/tts/twitter/model/User.java
@@ -1,6 +1,7 @@
 package com.tts.twitter.model;
 
 import java.util.Date;
+import java.util.List;
 import java.util.Set;
 
 import javax.persistence.CascadeType;
@@ -12,39 +13,60 @@ import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.JoinTable;
 import javax.persistence.ManyToMany;
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.Pattern;
 
 import org.hibernate.annotations.CreationTimestamp;
-
+import org.hibernate.validator.constraints.Length;
 
 @Entity
 public class User {
-	
+
 	@Id
 	@GeneratedValue(strategy = GenerationType.AUTO)
-	@Column(name="user_id")
+	@Column(name = "user_id")
 	private Long id;
 
+	@Email(message = "Please provide a valid email")
+	@NotEmpty(message = "Please provide an email")
 	private String email;
+
+	@Length(min = 3, message = "Your username must have at least 3 characters")
+	@Length(max = 15, message = "Your username cannot have more than 15 characters")
+	@Pattern(regexp = "[^\\s]+", message = "Your username cannot contain spaces")
 	private String username;
+
+	@Length(min = 5, message = "Your password must have at least 5 characters")
 	private String password;
+
+	@NotEmpty(message = "Please provide your first name")
 	private String firstName;
+
+	@NotEmpty(message = "Please provide your last name")
 	private String lastName;
+
 	private int active;
 
 	@CreationTimestamp
 	private Date createdAt;
 
 	@ManyToMany(cascade = CascadeType.ALL)
-	@JoinTable(name = "user_role", joinColumns = @JoinColumn(name = "user_id"), 
-    inverseJoinColumns = @JoinColumn(name = "role_id"))
+	@JoinTable(name = "user_role", joinColumns = @JoinColumn(name = "user_id"), inverseJoinColumns = @JoinColumn(name = "role_id"))
 	private Set<Role> roles;
 
-	//JPA NEEDS EMPTY CONSTRUCTOR
+	@ManyToMany(cascade = CascadeType.ALL)
+	@JoinTable(name = "user_follower", joinColumns = @JoinColumn(name = "user_id"), inverseJoinColumns = @JoinColumn(name = "follower_id"))
+	private List<User> followers;
+
+	@ManyToMany(mappedBy = "followers")
+	private List<User> following;
+
+	// JPA NEEDS EMPTY CONSTRUCTOR
 	public User() {
 	}
 
-	public User(Long id, String email, String username, String password, String firstName, String lastName, int active,
-			Date createdAt, Set<Role> roles) {
+	public User(Long id, String email, String username, String password, String firstName, String lastName, int active, Date createdAt, Set<Role> roles, List<User> followers, List<User> following) {
 		this.id = id;
 		this.email = email;
 		this.username = username;
@@ -54,85 +76,104 @@ public class User {
 		this.active = active;
 		this.createdAt = createdAt;
 		this.roles = roles;
+		this.followers = followers;
+		this.following = following;
+	}
+
+
+	public List<User> getFollowers() {
+		return this.followers;
+	}
+
+	public void setFollowers(List<User> followers) {
+		this.followers = followers;
+	}
+
+	public List<User> getFollowing() {
+		return this.following;
+	}
+
+	public void setFollowing(List<User> following) {
+		this.following = following;
 	}
 
 	public Long getId() {
 		return id;
 	}
-	
+
 	public void setId(Long id) {
 		this.id = id;
 	}
-	
+
 	public String getEmail() {
 		return email;
 	}
-	
+
 	public void setEmail(String email) {
 		this.email = email;
 	}
-	
+
 	public String getUsername() {
 		return username;
 	}
-	
+
 	public void setUsername(String username) {
 		this.username = username;
 	}
-	
+
 	public String getPassword() {
 		return password;
 	}
-	
+
 	public void setPassword(String password) {
 		this.password = password;
 	}
-	
+
 	public String getFirstName() {
 		return firstName;
 	}
-	
+
 	public void setFirstName(String firstName) {
 		this.firstName = firstName;
 	}
-	
+
 	public String getLastName() {
 		return lastName;
 	}
-	
+
 	public void setLastName(String lastName) {
 		this.lastName = lastName;
 	}
-	
+
 	public int getActive() {
 		return active;
 	}
-	
+
 	public void setActive(int active) {
 		this.active = active;
 	}
-	
+
 	public Date getCreatedAt() {
 		return createdAt;
 	}
-	
+
 	public void setCreatedAt(Date createdAt) {
 		this.createdAt = createdAt;
 	}
-	
+
 	public Set<Role> getRoles() {
 		return roles;
 	}
-	
+
 	public void setRoles(Set<Role> roles) {
 		this.roles = roles;
 	}
-	
+
 	@Override
 	public String toString() {
 		return "User [active=" + active + ", createdAt=" + createdAt + ", email=" + email + ", firstName=" + firstName
-				+ ", id=" + id + ", lastName=" + lastName + ", password=" + password + ", roles=" + roles + ", username="
-				+ username + "]";
+				+ ", id=" + id + ", lastName=" + lastName + ", password=" + password + ", roles=" + roles
+				+ ", username=" + username + "]";
 	}
-	
+
 }
